f47f93b9d0ae7b866de579a99cf1b8c4062e8849
Matt Crook <mattlcrook@icloud.com>
2020-11-27 22:52:27 +1300

    remove unused logger from home controller

--- Aoraki.Web/Controllers/HomeController.cs
+++ Aoraki.Web/Controllers/HomeController.cs
@@ -1,19 +1,11 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Aoraki.Web.Models.ViewModels;
 
 namespace Aoraki.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
