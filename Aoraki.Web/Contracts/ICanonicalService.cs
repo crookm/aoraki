270d2038d1845f7742508644d859863f1c9646f9
Matt Crook <mattlcrook@icloud.com>
2020-11-29 11:36:26 +1300

    implement canonicalise url service

---
+++ Aoraki.Web/Contracts/ICanonicalService.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Aoraki.Web.Contracts
+{
+    public interface ICanonicalService
+    {
+        string HostName { get; set; }
+        bool EnableTrailingSlash { get; set; }
+        bool EnableLowerCase { get; set; }
+
+        string CanonicaliseUrl(string url);
+        string CanonicaliseUrl(Uri uri);
+    }
+}
