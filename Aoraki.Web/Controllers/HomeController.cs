56640d38f330a7520e6c166a2df06ccb4141b0c9
Matt Crook <mattlcrook@icloud.com>
2020-11-28 13:20:08 +1300

    implement 404 error page

--- Aoraki.Web/Controllers/HomeController.cs
+++ Aoraki.Web/Controllers/HomeController.cs
@@ -11,5 +11,12 @@ namespace Aoraki.Web.Controllers
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
+
+        [HttpGet("/Home/Error/404")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error404()
+        {
+            return View("~/Views/Home/Error/Error404.cshtml");
+        }
     }
 }
