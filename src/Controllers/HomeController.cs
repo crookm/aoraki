07a1f7887f38d11b62027e3cd2ba370e995f601d
Matt Crook <mattlcrook@icloud.com>
2022-02-11 21:49:46 +1300

    update generic error page, exclude from code coverage

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -1,21 +1,18 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using Aoraki.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aoraki.Web.Controllers;
 
+[ExcludeFromCodeCoverage] // Remove exclusion if endpoints with something worth testing are added
 public class HomeController : Controller
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+        => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 
     [HttpGet("/Home/Error/404")]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error404()
-    {
-        return View("~/Views/Home/Error/Error404.cshtml");
-    }
+    public IActionResult Error404() => View("~/Views/Home/Error/Error404.cshtml");
 }
