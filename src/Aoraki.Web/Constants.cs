3339a7b052c4daf2ffdd65f4296a4dc2c59df8d5
Matt Crook <mattlcrook@icloud.com>
2022-02-18 18:52:48 +1300

    adjust site host and name

--- src/Aoraki.Web/Constants.cs
+++ src/Aoraki.Web/Constants.cs
@@ -2,8 +2,8 @@ namespace Aoraki.Web;
 
 public static class Constants
 {
-    public const string SiteBaseUrl = "https://crookm.com";
-    public const string SiteHostName = "crookm.com";
-    public const string SiteTitle = "Matts Blog";
+    public const string SiteBaseUrl = "https://mattcrook.io";
+    public const string SiteHostName = "mattcrook.io";
+    public const string SiteTitle = "Matt Crook";
     public const string SiteFeedBaseId = "uuid:b8787de3-c2eb-41bc-89ab-9c176300d44c"; // Not to be changed
 }
